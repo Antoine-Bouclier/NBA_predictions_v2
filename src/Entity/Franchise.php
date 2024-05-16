e0f44e9c9c089f0045ff23a142534b9413b61b2c
Antoine-Bouclier <antoinebouclier1@gmail.com>
2024-05-16 11:08:55 +0200

    Create Franchise entity

---
+++ src/Entity/Franchise.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FranchiseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FranchiseRepository::class)]
+class Franchise
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $town = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $logo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $conference = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTown(): ?string
+    {
+        return $this->town;
+    }
+
+    public function setTown(string $town): static
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): static
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getConference(): ?string
+    {
+        return $this->conference;
+    }
+
+    public function setConference(string $conference): static
+    {
+        $this->conference = $conference;
+
+        return $this;
+    }
+}
